d205087486348988498315d592e5821ec9cae11a
Hoyoung Jung <honux77@gmail.com>
2019-01-07 18:30:35 +0900

    Init Spring boot
    start.spring.io를 사용한 초기화

---
+++ baek1/src/main/java/honux/baek1/Baek1Application.java
@@ -0,0 +1,14 @@
+package honux.baek1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Baek1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Baek1Application.class, args);
+	}
+
+}
+
